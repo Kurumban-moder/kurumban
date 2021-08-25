ac432e64b9110cc9dec8859e74d398981ceacdea
BADBOY MODER <88477572+BADBMODER@users.noreply.github.com>
2021-08-25 17:24:31 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,19 +11,19 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://www.pixelstalk.net/wp-content/uploads/2016/03/Pikachu-wallpaper-HD-dowload.jpg";
+  r_text[0] = "https://i.imgur.com/MizzftB.jpeg";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Ameer Suhail*
-*owner number wa.me/919946432377*
-*bot setting video : https://youtu.be/_D4ZYuUSXjs
-*githublink       https://github.com/ameer-kallumthodi/pikachu*
-*audio commads    https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
-*sticker commads  https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Badboy*
+*owner number wa.me/918593079460*
+*bot setting video : https://youtu.be/fBNiy3nfcgg
+*githublink       https://github.com/BADBMODER/Badboy*
+*audio commads    https://github.com/BADBMODER/Badboy/tree/master/uploads*
+*sticker commads  https://github.com/BADBMODER/Badboy/tree/master/uploads*
 `}) 
 
 }));
