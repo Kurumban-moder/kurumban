f0e1c0b3b381a65ca16892ab8352db45a83413c5
BADBOY MODER <88477572+BADBMODER@users.noreply.github.com>
2021-08-27 13:45:17 +0530

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -41,7 +41,7 @@ Asena.addCommand({pattern: 'tagall ?(.*)', fromMe: true, dontAddCommandList: tru
             mesaj = '';
             grup['participants'].map(
                 async (uye) => {
-                    mesaj += '\n ✨️ @' + uye.id.split('@')[0] + 'ㅤ💞';
+                    mesaj += '\n chumma text cheytha @' + uye.id.split('@')[0] + 'ㅤchudavalle';
                     jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
                 }
             );
